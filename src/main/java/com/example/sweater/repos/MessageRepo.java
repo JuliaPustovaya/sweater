6ee55e5417152f824dbf4fe23f61f710636b0581
JuliaPustovaya <pustovayajulia@gmail.com>
2018-10-01 17:37:02 +0300

    Adding db

---
+++ src/main/java/com/example/sweater/repos/MessageRepo.java
@@ -0,0 +1,12 @@
+package com.example.sweater.repos;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.sweater.domain.Message;
+
+public interface MessageRepo extends CrudRepository<Message,Long> {
+	List<Message> findByTag(String tag);
+	
+}
